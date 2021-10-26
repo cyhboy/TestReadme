08b45a4e0a4cd85dc9118605a2cee9c887162725
cyh <cyhboy@tom.com>
2021-10-26 15:18:34 +0800

    Initial Commit

---
+++ TestReadme/TestReadmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestReadmeApp.swift
+//  TestReadme
+//
+//  Created by hws on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TestReadmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
